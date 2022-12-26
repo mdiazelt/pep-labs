90e2cb2b794fdfe7c51b1201fac962df76bda195
mdiazelt <roshayo@yahoo.com>
2022-12-26 20:38:03 +0000

    Saved progress on ArraySum 2022-12-26 20:38:03 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,11 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        int total = 0;
+        
+        for(int i = 0; i < arr.length; i++){
+            total += arr[i];
+        }
+        return total;
     }
 }
