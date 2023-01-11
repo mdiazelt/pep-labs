a0c37a22ad1921f455e367e3020182aec5b672f1
mdiazelt <roshayo@yahoo.com>
2023-01-11 18:07:07 +0000

    Saved progress on Pyramid 2023-01-11 18:07:07 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -27,6 +27,11 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+               return ;
+            }
+        }
         return "";
     }
 }
