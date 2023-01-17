b1afcd06d257fa9269d10435f5fdd276cd868ed1
mdiazelt <roshayo@yahoo.com>
2023-01-17 19:17:47 +0000

    Saved progress on Library 2023-01-17 19:17:47 +0000

--- Library/src/main/java/Application/DAO/AuthorDAO.java
+++ Library/src/main/java/Application/DAO/AuthorDAO.java
@@ -56,7 +56,7 @@ public class AuthorDAO {
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString method here.
-            preparedStatement.setString(1, );
+            preparedStatement.setString(1, getAllAuthors());
 
             preparedStatement.executeUpdate();
             ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
