faf21fa0ca003f986d1c5d657d7c80adc0dfb0d0
mdiazelt <roshayo@yahoo.com>
2023-01-17 23:08:36 +0000

    Saved progress on RequestBody 2023-01-17 23:08:36 +0000

--- Library/src/main/java/Application/DAO/BookDAO.java
+++ Library/src/main/java/Application/DAO/BookDAO.java
@@ -32,7 +32,7 @@ public class BookDAO {
         List<Book> books = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM Book";//"change me";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -57,10 +57,11 @@ public class BookDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM Book WHERE book_id = ?";//"change me";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
             //write preparedStatement's setInt method here.
+            PreparedStatement.setInt(1, Book.id);
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
