20608dfa0bc73dc8ebdc6c66a5f08897e03a9bd5
mdiazelt <roshayo@yahoo.com>
2023-01-27 20:10:42 +0000

    Saved progress on SubArray 2023-01-27 20:10:42 +0000

--- SubArray/src/main/java/SubArray.java
+++ SubArray/src/main/java/SubArray.java
@@ -1,3 +1,4 @@
+import java.util.stream.IntStream;
 
 public class SubArray {
     /**
@@ -13,6 +14,11 @@ public class SubArray {
      * @return a sub-array of nums containing the values between start and end.
      */
     public int[] sub(int[] nums, int start, int end){
-        return null;
+
+        int[] subNums = IntStream.range(start, end)
+        .map(i -> nums[i])
+        .toArray();
+
+        return subNums;
     }
 }
