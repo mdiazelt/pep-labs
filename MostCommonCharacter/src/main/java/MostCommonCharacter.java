696055aaade87e146f9f9bf38ea2b6dc01300cf9
mdiazelt <roshayo@yahoo.com>
2023-01-27 19:34:00 +0000

    Saved progress on MostCommonCharacter 2023-01-27 19:34:00 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -10,7 +10,26 @@ public class MostCommonCharacter {
      */
     public char recurringChar(String str) {
 
-    return 0;
+        Map<Character, Integer> charcterCount = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i ++) {
+            char c = str.charAt(i);
+            if(!charcterCount.containsKey(c)){
+                charcterCount.put(c, 0);    
+            }
+            charcterCount.put(c, charcterCount.get(c) + 1);
+            
+        } 
+        int count = 0;
+        char c= str.charAt(0);
+        for(Map.Entry<Character, Integer> entry : charcterCount.entrySet()){
+            if(entry.getValue() > count){
+                count = entry.getValue();
+                c = entry.getKey();
+            }
+
+        }
+        return c;
     }
     
 }
